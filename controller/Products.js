3fea6431030b35223642c93c3785168c4783985e
prashant <mohitvishwakarma24092001@gmail.com>
2025-05-15 23:59:48 +0530

    done

--- controller/Products.js
+++ controller/Products.js
@@ -11,5 +11,15 @@ export const createProduct = async (req, res) => {
     res.status(400).json(error);
   }
 };
+export const fetchAllProducts = async (req, res) => {
+  // this product we have to get from API body
+  // here we need all query string
+  const products = Product.find();
 
-
+  try {
+    const response = await product.save();
+    res.status(201).send(response);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
