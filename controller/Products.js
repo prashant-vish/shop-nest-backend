04f8d045abbc58fd702a00399ef2b8593325ae81
prashant <mohitvishwakarma24092001@gmail.com>
2025-05-22 21:11:17 +0530

    Added Fetch all Product Route with filter

--- controller/Products.js
+++ controller/Products.js
@@ -12,13 +12,43 @@ export const createProduct = async (req, res) => {
   }
 };
 export const fetchAllProducts = async (req, res) => {
-  // this product we have to get from API body
-  // here we need all query string
-  const products = Product.find();
+  // filter ={"category":["smartphone","beauty"],"brand":["apple","acer"]}
+  // sort ={"_sort":"price"}
+  // pagination ={_page:1,_per_page:1}
+  // Todo : We have to try with multiple Category and brand after change in frontend.
+
+  let query = Product.find({});
+  let totalProductsQuery = Product.find({});
+
+  if (req.query.category) {
+    query = query.find({ category: req.query.category });
+    totalProductsQuery = totalProductsQuery.find({
+      category: req.query.category,
+    });
+  }
+
+  if (req.query.brand) {
+    query = query.find({ brand: req.query.brand });
+    totalProductsQuery = totalProductsQuery.find({ brand: req.query.brand });
+  }
+  if (req.query._sort && req.query._order) {
+    totalProductsQuery = totalProductsQuery.sort({
+      [req.query._sort]: req.query._order,
+    });
+  }
+  const totalDocs = await totalProductsQuery.countDocuments().exec();
+  console.log({ totalDocs });
+
+  if (req.query._page && req.query._per_page) {
+    const pageSize = req.query._per_page;
+    const page = req.query._page;
+    query = query.skip(pageSize * (page - 1)).limit(pageSize);
+  }
 
   try {
-    const response = await product.save();
-    res.status(201).send(response);
+    const docs = await query.exec();
+    res.set("X-Total-Count", totalDocs);
+    res.status(201).send(docs);
   } catch (error) {
     res.status(400).json(error);
   }
--- routes/Products.js
+++ routes/Products.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createProduct } from "../controller/Products.js";
+import { createProduct, fetchAllProducts } from "../controller/Products.js";
 
 const router = express.Router();
 
 router.post("/", createProduct);
+router.get("/", fetchAllProducts);
 
 export default router;
